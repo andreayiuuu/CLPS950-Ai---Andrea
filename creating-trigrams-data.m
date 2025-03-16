1f516bd1f95857dd439d66b02be5d5a8dc5c7cd8
andreayiuuu <andrea_yiu@brown.edu>
2025-03-16 15:24:51 -0400

    Update creating-trigrams-data.m

--- creating-trigrams-data.m
+++ creating-trigrams-data.m
@@ -5,6 +5,7 @@ if ischar(filename)
     filename = {filename};
 end
 
+total_trigrams = 0;
 for i = 1:length(filename)
 file_path = fullfile(location, filename{i}); %gives full link of file and location%
 text = fileread(file_path); %reads the text%
@@ -12,9 +13,23 @@ text = lower(text); %lowercase, important because the trigrams should not be cas
 text = regexprep(text, '[^a-z]', ''); %makes the text only letters not sure if this connects words separated by spaces%
 for j = 1:1:length(text)-2 %from j being the first letter to being the third last letter because trigram is three letters%
 trigram = text(j:j+2);
-disp(trigram)
+if isKey(count_trigrams, trigram)
+count_trigrams(trigram) = count_trigrams(trigram) + 1; 
+else 
+count_trigrams(trigram) = 1 
+end
+total_trigrams = total_trigrams + 1;
 end
 end
+
+trigram_freq = container.Map('KeyType', 'char', 'ValueType', 'double')
+keys_List = key(count_trigrams); 
+for i = 1:length(key_list);
+trigram_freq = count_trigrams(trigram)/total_trigrams;
+end
+
+disp(trigram_freq)
+
 
 
 
